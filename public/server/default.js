ebc0cdad8c3e234ead30e010252bba4ac4102aaa
Wildhoney <adam.timberlake@gmail.com>
2017-01-06 12:06:25 +0000

    Added server

---
+++ public/server/default.js
@@ -0,0 +1,14 @@
+import { basename } from 'path';
+import { existsSync } from 'fs';
+import http from 'http';
+import express from 'express';
+import opener from 'opener';
+
+const app = express();
+const server = http.createServer(app);
+const isHeroku = 'HEROKU_APP_NAME' in process.env;
+
+app.use(express.static(__dirname + '/example'));
+
+server.listen(process.env.PORT || 5000);
+!isHeroku && opener('http://localhost:5000');
