bd202788d6bc8e24e9a5008fd0c422d4248ce3be
Wildhoney <adam.timberlake@gmail.com>
2017-01-07 07:50:18 +0000

    Moved server devDependencies into dependencies

--- public/server/default.js
+++ public/server/default.js
@@ -3,12 +3,19 @@ import { existsSync } from 'fs';
 import http from 'http';
 import express from 'express';
 import opener from 'opener';
+import { get } from 'axios';
 
 const app = express();
 const server = http.createServer(app);
 const isHeroku = 'HEROKU_APP_NAME' in process.env;
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/authenticate', (req, res) => {
+
+    res.send('Instamap');
+
+});
 
 server.listen(process.env.PORT || 5000);
 !isHeroku && opener('http://127.0.0.1:5000');
