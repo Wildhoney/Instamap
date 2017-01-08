86bc2a95ff4a94cbcd9e39d4a48e942840822c81
Wildhoney <adam.timberlake@gmail.com>
2017-01-08 17:55:39 +0000

    Improved tests

--- src/components/map/default.test.js
+++ src/components/map/default.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import test from 'ava';
 import { shallow } from 'enzyme';
+import Search from '../search/default';
 import Map from './default';
 
 test('It should be able to render the map;', t => {
     const wrapper = shallow(<Map />);
     t.is(wrapper.find('section.map').length, 1);
-    t.is(wrapper.find('input').length, 1);
-    t.is(wrapper.find('button').length, 1);
+    t.is(wrapper.find(Search).length, 1);
 });
--- src/components/search/default.test.js
+++ src/components/search/default.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import test from 'ava';
 import { shallow } from 'enzyme';
-import Map from './default';
+import Search from './default';
 
-test('It should be able to render the map;', t => {
-    const wrapper = shallow(<Map />);
-    t.is(wrapper.find('section.map').length, 1);
+test('It should be able to render the search field;', t => {
+    const wrapper = shallow(<Search />);
     t.is(wrapper.find('input').length, 1);
     t.is(wrapper.find('button').length, 1);
 });
